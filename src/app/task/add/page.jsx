a0d9088a20b040db1badfd0707cefa9e7507cac5
mallexibra <mallexibra@gmail.com>
2023-09-06 09:32:54 +0700

    chors(task): fixing page to add task

--- src/app/task/add/page.jsx
+++ src/app/task/add/page.jsx
@@ -6,35 +6,39 @@ const AddTask = () => {
     <main>
       <Header page={"Tambah Program Kerja"} />
       <div className="card">
-        <form className="space-y-2" action="">
-          <LabelForm name={"Nama Program Kerja"} type={"text"}>
-            Nama program kerja anda...
-          </LabelForm>
-          <LabelForm name={"Deskripsi"} longText={true} type={"text"}>
-            Deskripsi program kerja anda...
-          </LabelForm>
-          <LabelForm name={"Tujuan"} longText={true} type={"text"}>
-            Tujuan program kerja anda...
-          </LabelForm>
-          <LabelForm name={"Sasaran"} longText={true} type={"text"}>
-            Sasaran program kerja anda...
-          </LabelForm>
-          <LabelForm name={"Tanggal Rencana"} type={"text"}>
-            Tanggal rencana program kerja anda...
-          </LabelForm>
-          <LabelForm name={"Penyelenggara"} type={"text"}>
-            Penyelenggara program kerja anda...
-          </LabelForm>
-          <LabelForm name={"Sumber Dana"} type={"text"}>
-            Sumber dana program kerja anda...
-          </LabelForm>
-          <LabelForm name={"Jumlah Anggaran"} type={"text"}>
-            Julah anggaran program kerja anda...
-          </LabelForm>
-          <LabelForm name={"Tahun Anggaran"} type={"text"}>
-            Tahun anggaran program kerja anda...
-          </LabelForm>
-          <button className="button w-full" type="submit">
+        <form className="space-y-2 md:grid gap-3 md:grid-cols-2" action="">
+          <div>
+            <LabelForm name={"Nama Program Kerja"} type={"text"}>
+              Nama program kerja anda...
+            </LabelForm>
+            <LabelForm name={"Deskripsi"} longText={true} type={"text"}>
+              Deskripsi program kerja anda...
+            </LabelForm>
+            <LabelForm name={"Tujuan"} longText={true} type={"text"}>
+              Tujuan program kerja anda...
+            </LabelForm>
+            <LabelForm name={"Sasaran"} type={"text"}>
+              Sasaran program kerja anda...
+            </LabelForm>
+          </div>
+          <div>
+            <LabelForm name={"Tanggal Rencana"} type={"text"}>
+              Tanggal rencana program kerja anda...
+            </LabelForm>
+            <LabelForm name={"Penyelenggara"} type={"text"}>
+              Penyelenggara program kerja anda...
+            </LabelForm>
+            <LabelForm name={"Sumber Dana"} type={"text"}>
+              Sumber dana program kerja anda...
+            </LabelForm>
+            <LabelForm name={"Jumlah Anggaran"} type={"text"}>
+              Julah anggaran program kerja anda...
+            </LabelForm>
+            <LabelForm name={"Tahun Anggaran"} type={"text"}>
+              Tahun anggaran program kerja anda...
+            </LabelForm>
+          </div>
+          <button className="button w-full col-span-2" type="submit">
             Tambahkan Program Kerja
           </button>
         </form>
--- src/components/LabelForm.jsx
+++ src/components/LabelForm.jsx
@@ -1,14 +1,25 @@
-const LabelForm = ({ name, type, children }) => {
+const LabelForm = ({ name, type, children, longText }) => {
   return (
     <label className="flex flex-col" htmlFor={name}>
       <span className="font-medium">{name}</span>
-      <input
-        placeholder={children}
-        className="p-2 placeholder:text-xs outline-none border border-primary-color text-primary-color bg-second-color rounded-md "
-        type={type}
-        name={name}
-        id={name}
-      />
+      {longText ? (
+        <textarea
+          placeholder={children}
+          className="p-2 placeholder:text-xs outline-none border border-primary-color text-primary-color bg-second-color rounded-md "
+          name={name}
+          id={name}
+          cols="30"
+          rows="3"
+        ></textarea>
+      ) : (
+        <input
+          placeholder={children}
+          className="p-2 placeholder:text-xs outline-none border border-primary-color text-primary-color bg-second-color rounded-md "
+          type={type}
+          name={name}
+          id={name}
+        />
+      )}
     </label>
   );
 };
