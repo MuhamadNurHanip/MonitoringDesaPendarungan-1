1770652b23c7f0850df1677ce49c1770620c8196
mallexibra <mallexibra@gmail.com>
2023-09-06 21:01:22 +0700

    chors(money): add money page

---
+++ src/app/money/SumberDana.jsx
@@ -0,0 +1,62 @@
+"use client";
+import LabelForm from "@/components/LabelForm";
+import Image from "next/image";
+
+const SumberDana = () => {
+  return (
+    <section className="card">
+      <h3 className="title-section">Sumber Dana</h3>
+      <div>
+        <ol className="list-decimal pl-3 font-medium">
+          <li>
+            <div className="flex justify-between items-center">
+              <p>Kementrian Keuangan</p>
+              <div className="flex gap-3">
+                <button
+                  className="cursor-pointer"
+                  onClick={() => window.modal.showModal()}
+                >
+                  <Image
+                    width={20}
+                    height={20}
+                    alt="edit-icon"
+                    src={"/edit.svg"}
+                  />
+                </button>
+                <Image
+                  width={18}
+                  height={18}
+                  alt="trash-icon"
+                  src={"/trash.svg"}
+                />
+              </div>
+            </div>
+          </li>
+        </ol>
+      </div>
+      <button
+        className="outline-none font-semibold mt-3 button"
+        onClick={() => window.modal.showModal()}
+      >
+        Tambah Sumber Dana
+      </button>
+      <dialog id="modal" className="modal overflow-y-scroll">
+        <div className="modal-box max-w-2xl mx-3 md:w-2/5 w-11/12 no-scrollbar bg-white">
+          <form action="">
+            <LabelForm name={"Nama Sumber Dana"} type={"text"}>
+              Nama sumber dana
+            </LabelForm>
+            <button className="button mt-3 w-full font-semibold" type="submit">
+              Tambah Sumber Dana
+            </button>
+          </form>
+        </div>
+        <form method="dialog" className="modal-backdrop h-full">
+          <button>close</button>
+        </form>
+      </dialog>
+    </section>
+  );
+};
+
+export default SumberDana;
--- src/app/money/page.jsx
+++ src/app/money/page.jsx
@@ -1,7 +1,17 @@
 import Header from "@/components/Header";
+import Money from "@/components/Money";
+import SumberDana from "./SumberDana";
 
-const Money = () => {
-  return <Header page={"Keuangan Desa"} />;
+const MoneyPage = () => {
+  return (
+    <main>
+      <Header page={"Keuangan Desa"} />
+      <div className="space-y-3 md:flex gap-3">
+        <Money />
+        <SumberDana />
+      </div>
+    </main>
+  );
 };
 
-export default Money;
+export default MoneyPage;
