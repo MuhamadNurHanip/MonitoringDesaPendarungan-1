1a477c09a696b17c70df0eb095adeabd981e8394
mallexibra <mallexibra@gmail.com>
2023-09-17 20:25:18 +0700

    chors(statistik): consume API for statistik

--- src/components/Statistik.jsx
+++ src/components/Statistik.jsx
@@ -1,14 +1,59 @@
+"use client";
+
+import { hitungPercent } from "@/lib/setPercent";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const Statistik = () => {
+  const [jumlahProker, setJumlahProker] = useState({ jumlah: 0, persen: 0 });
+  const [progressProker, setProgressProker] = useState({
+    jumlah: 0,
+    persen: 0,
+  });
+  const [finishProker, setFinishProker] = useState({ jumlah: 0, persen: 0 });
+
+  const getStatistik = async () => {
+    try {
+      const data = (
+        await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/proker`)
+      ).data.data;
+      setJumlahProker({ jumlah: data.length, persen: 100 });
+      let progress = 0;
+      let finish = 0;
+      data.map((item) => {
+        if (item.status == "Progress") {
+          progress++;
+        } else if (item.status == "Selesai") {
+          finish++;
+        }
+      });
+      setProgressProker({
+        jumlah: progress,
+        persen: hitungPercent(progress, jumlahProker.jumlah),
+      });
+      setFinishProker({
+        jumlah: finish,
+        persen: hitungPercent(finish, jumlahProker.jumlah),
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getStatistik();
+  }, []);
+
   return (
     <section data-aos="fade-down-left" className="card my-3">
       <h1 className="title-section">Statistik Desa</h1>
       <div>
         <p>Program Kerja</p>
         <div className="flex items-center gap-3 my-1">
-          <p className="text-lg font-bold">23</p>
+          <p className="text-lg font-bold">{jumlahProker.jumlah}</p>
           <progress
             className="progress progress-info w-56"
-            value="100"
+            value={jumlahProker.persen}
             max="100"
           ></progress>
         </div>
@@ -16,10 +61,10 @@ const Statistik = () => {
       <div>
         <p>Progress Program Kerja</p>
         <div className="flex items-center gap-3 my-1">
-          <p className="text-lg font-bold">15</p>
+          <p className="text-lg font-bold">{progressProker.jumlah}</p>
           <progress
             className="progress progress-warning w-56"
-            value="60"
+            value={progressProker.persen}
             max="100"
           ></progress>
         </div>
@@ -27,10 +72,10 @@ const Statistik = () => {
       <div>
         <p>Program Kerja Selesai</p>
         <div className="flex items-center gap-3 my-1">
-          <p className="text-lg font-bold">3</p>
+          <p className="text-lg font-bold">{finishProker.jumlah}</p>
           <progress
             className="progress progress-success w-56"
-            value="20"
+            value={finishProker.persen}
             max="100"
           ></progress>
         </div>
