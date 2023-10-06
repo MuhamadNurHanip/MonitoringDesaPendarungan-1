1b8c543326fc1d5e9bf9e0e879152ce4b43f7d73
mallexibra <mallexibra@gmail.com>
2023-10-06 21:23:11 +0700

    chors(icon): resize icon search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,7 +6,7 @@ const Search = ({ search }) => {
       className="flex gap-3 w-max text-xs rounded-md p-2 border border-primary-color"
       htmlFor="search"
     >
-      <Image width={24} height={24} alt="Search-icon" src={"/search.svg"} />
+      <Image width={16} height={16} alt="Search-icon" src={"/search.svg"} />
       <input
         className="bg-second-color outline-none"
         placeholder="Cari program kerja"
